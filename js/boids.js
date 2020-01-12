fd7e3e22510964f880f057cab13b90c129965ee0
deev <devolved@gmail.com>
2020-01-12 20:47:51 +0000

    basic gravity

--- js/boids.js
+++ js/boids.js
@@ -1,6 +1,5 @@
 
 var canvas = document.getElementById('boidCanvas');
-console.log(canvas);
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -8,7 +7,7 @@ canvas.height = window.innerHeight;
 var w  = window.innerWidth
 var h = window.innerHeight;
 var c = canvas.getContext('2d');
-
+var g = 4;
 
 function rnd(min, max) {
     min = Math.ceil(min);
@@ -17,7 +16,7 @@ function rnd(min, max) {
 }
 
 function rndColour() {
-    var col = 'rgba(' + rnd(150, 255) + ', ' + rnd(150, 255) + ', ' + rnd(150, 255) + ', 0.8)'; 
+    var col = 'rgba(' + rnd(150, 255) + ', ' + rnd(150, 255) + ', ' + rnd(150, 255) + ', 1)'; 
     return col;
 }
 
@@ -26,9 +25,9 @@ function Circle(){
     this.r = 6;
 
     this.x = rnd((this.r * 2), (w - (this.r * 2)));
-    this.y = rnd((this.r * 2), (h - (this.r * 2)));
-    this.dx = rnd(1, 5);
-    this.dy = rnd(1, 5);
+    this.y = rnd((h / 2), this.r);
+    this.dx = rnd(1, 6);
+    this.dy = rnd(1, 6);
     this.colour = rndColour();
     
 
@@ -47,36 +46,49 @@ function Circle(){
         {
             this.dx = -this.dx;
         }
-    
+        this.x += this.dx;
+
+        /* gravity */
+
+        // y bounds
         if(this.y + this.r > h || this.y - this.r < 0)
         {
             this.dy = -this.dy;
+            
         }
-    
-        this.x += this.dx;
-        this.y += this.dy;
 
+        this.dy = this.dy - g;
+
+        this.y -= this.dy;
+        
+
+               
+  
+
+
+  
         this.draw();
     }
 
 }
 
+
+// generate circles
 var circles = [];
 
-for (let i = 0; i < 999; i++) {
+for (let i = 0; i < 21; i++) {
     var circ = new Circle();
     circles.push(circ);
     
 }
 
-console.log(circles.length);
 
-
+// animate loop
 var animate = function() { 
     requestAnimationFrame(animate);
     c.clearRect(0, 0 , w, h);
 
-   // console.warn('%c < %cWee ', 'color:lime;background:black','color:pink;background:black;font-weight:bolder');
+   console.log(circles[0].dy);
 
 
    for (let i = 0; i < circles.length; i++) {
